1d7c859dce08d10c70370c847b72049a761c8a4c
Declan Frye <deckleberryfrye@gmail.com>
2013-06-12 10:14:37 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'haml'
+
+group :test, :development do
+  gem 'rspec'
+  gem 'linecache19', :git => "git://github.com/mark-moseley/linecache"
+  gem 'ruby-debug-base19x', '~> 0.11.30.pre4'
+  gem 'ruby-debug19'
+end
---
+++ app.rb
@@ -0,0 +1,11 @@
+Dir['./config/**/*.rb'].each { |f| require f }
+Dir['./lib/**/*.rb'].each { |f| require f }
+
+require 'sinatra'
+
+get '/' do
+  haml :index
+end
+
+# Copy the next line into spec/spec_helper.rb
+# require File.expand_path('app', File.dirname(File.dirname(__FILE__)))
