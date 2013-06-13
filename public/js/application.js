51e4385b7fc9be2f2144a140b617b972a5343efb
Declan Frye <deckleberryfrye@gmail.com>
2013-06-13 14:46:50 -0500

    started writing ScreenSaver code

--- public/js/application.js
+++ public/js/application.js
@@ -4,83 +4,109 @@
  */
 
 var ScreenSaver = function(selector) {
-    this.el = $(selector);
-    var offset = this.el.offset();
-    this.top = offset.top
+    this.element = $(selector);
+    var offset = this.element.offset();
+    this.top = offset.top;
     this.left = offset.left;
-}
+    this.width = this.element.width();
+    this.height = this.element.height();
 
-var record_offset = function() {
-  offset = $('img').offset();
-  Mooninites.offset_top = offset.top;
-  Mooninites.offset_left = offset.left;
-}
-
-var get_offset = function() {
-  return $('img').offset();
-};
-
-var set_offset = function(offset_top, offset_left) {
-  return $('img').offset({top: offset_top, left: offset_left});
-};
-
-var log_offset = function() {
-  console.log(get_offset());
-}
-
-var log_offset_loop = function() {
-  //log_offset();
-  console.log($('img').offset());
-  setTimeout(log_offset_loop, 5000);
-  return null
-}
-
-var log_window_size = function() {
-  console.log($(window).height(),', ', $(window).width());
-}
-
-var width = $(window).width();
-var height = $(window).height();
-var going_down = false
-var going_right = true
-var drift = function() {
-    record_offset();
-
-    // Going down
-    if (Mooninites.offset_top + 230 < height && going_down) {
-        if (Mooninites.offset_left + 300 < width && going_right) {
-            // Going down and right
-            set_offset(Mooninites.offset_top + 1, Mooninites.offset_left + 1);
-        }
-        else {
-            // Going down and left
-            set_offset(Mooninites.offset_top + 1, Mooninites.offset_left -1);
-        }
-    }
-    // Going up
-    else {
-        if (Mooninites.offset_left +300 < width && going_right) {
-            // Going up and right
-            set_offset(Mooninites.offset_top -1, Mooninites.offset_left + 1);
-        }
-        else {
-            // Going up and left
-            set_offset(Mooninites.offset_top -1, Mooninites.offset_left -1);
-        }
+    this.setPosition = function(offset_top, offset_left) {
+      this.top = offset_top;
+      this.left = offset_left;
+      this.element.offset({top:offset_top, left:offset_left});
     }
 
-    // Set direction for next moonimove
-    if (Mooninites.offset_top + 230 == height)
-        going_down = false
-    if (Mooninites.offset_left + 300 == width)
-        going_right = false
-    if (Mooninites.offset_top == 0)
-        going_down = true
-    if (Mooninites.offset_left == 0)
-        going_right = true
-    record_offset();
-    setTimeout(drift, 10);
+    this.move = function() {
+      var new_top = this.getNewTop();
+      var new_left = this.getNewLeft();
+      this.setPosition(new_top, new_left);
+    };
+
+    this.getNewTop = function() {
+      return this.top + 1;
+    }
+
+    this.getNewLeft = function() {
+      return this.left + 1;
+    }
 }
 
+/*
+var drift;
+$(document).ready(function() {
 
+  var Mooninites = new ScreenSaver('img');
+  var width = $(window).width();
+  var height = $(window).height();
+  var going_down = false;
+  var going_right = true;
 
+  var record_offset = function() {
+    offset = $('img').offset();
+    Mooninites.top = offset.top;
+    Mooninites.left = offset.left;
+  }
+
+  var get_offset = function() {
+    return $('img').offset();
+  };
+
+  var set_offset = function(offset_top, offset_left) {
+    return $('img').offset({top: offset_top, left: offset_left});
+  };
+
+  var log_offset = function() {
+    console.log(get_offset());
+  }
+
+  var log_offset_loop = function() {
+    console.log($('img').offset());
+    setTimeout(log_offset_loop, 5000);
+    return null
+  }
+
+  var log_window_size = function() {
+    console.log($(window).height(),', ', $(window).width());
+  }
+
+  var drift = function() {
+      record_offset();
+
+      // Going down
+      if (Mooninites.top + Mooninites.height < height && going_down) {
+          if (Mooninites.left + Mooninites.width < width && going_right) {
+              // Going down and right
+              set_offset(Mooninites.top + 1, Mooninites.left + 1);
+          }
+          else {
+              // Going down and left
+              set_offset(Mooninites.top + 1, Mooninites.left -1);
+          }
+      }
+      // Going up
+      else {
+          if (Mooninites.left + Mooninites.width < width && going_right) {
+              // Going up and right
+              set_offset(Mooninites.top -1, Mooninites.left + 1);
+          }
+          else {
+              // Going up and left
+              set_offset(Mooninites.top -1, Mooninites.left -1);
+          }
+      }
+
+      // Set direction for next moonimove
+      if (Mooninites.top + Mooninites.height == height)
+          going_down = false
+      if (Mooninites.left + Mooninites.width == width)
+          going_right = false
+      if (Mooninites.top == 0)
+          going_down = true
+      if (Mooninites.left == 0)
+          going_right = true
+      record_offset();
+      setTimeout('window.drift', 10);
+  }();
+});
+*/
--- public/js/test.js
+++ public/js/test.js
@@ -3,8 +3,56 @@
  */
 
 describe('ScreenSaver', function() {
-  it('finds the offset of its DOM element', function() {
-    screenSaver = new ScreenSaver('#test-screensaver');
+  var screenSaver;
+  var screenSaverDOM;
+
+  beforeEach(function() { 
+    screenSaver = new ScreenSaver('#test-screensaver'); 
+    screenSaverDOM = $('#test-screensaver');
+  });
+
+  it('finds the offset top of its DOM element', function() {
     assert.equal(screenSaver.top, 50);
   });
+
+  it('finds the offset left of its DOM element', function() {
+    assert.equal(screenSaver.left, 50);
+  });
+
+  it('knows the width of its DOM element', function() {
+    assert.equal(screenSaver.width, 300);
+  });
+
+  it('knows the height of its DOM element', function() {
+    assert.equal(screenSaver.height, 200);
+  });
+
+  describe('#setPosition()', function() {
+    beforeEach(function() {
+      screenSaver.setPosition(600,700);
+    });
+    it('sets screenSaver.top', function() {
+      assert.equal(screenSaver.top, 600);
+    });
+    it('sets screenSaver.left', function() {
+      assert.equal(screenSaver.left, 700);
+    });
+    it('sets screenSaver.top', function() {
+      assert.equal(screenSaverDOM.offset().top, 600);
+    });
+    it('sets screenSaver.left', function() {
+      assert.equal(screenSaverDOM.offset().left, 700);
+    });
+  });
+
+  describe('#move()', function() {
+    it('moves the screen saver down and right from its starting position', function() {
+      var starting_top = screenSaver.top;
+      var starting_left = screenSaver.left;
+      screenSaver.move();
+      assert.equal(screenSaver.top, starting_top + 1);
+      assert.equal(screenSaver.left, starting_left + 1);
+    });
+  });
+
 });
