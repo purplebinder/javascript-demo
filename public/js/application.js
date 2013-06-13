77fcaf1a32aa5047ae0ac1281933be2ee94fec62
tag0mag0 <jem.hilton@gmail.com>
2013-06-12 21:39:54 -0500

    completed js drift action

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,20 @@
-var WhiteSquare = {
+var Mooninites = {
     offset_top: 0,
     offset_left: 0
 }
 
 var record_offset = function() {
-  offset = $('#white-square').offset();
-  WhiteSquare.offset_top = offset.top;
-  WhiteSquare.offset_left = offset.left;
+  offset = $('img').offset();
+  Mooninites.offset_top = offset.top;
+  Mooninites.offset_left = offset.left;
 }
 
 var get_offset = function() {
-  return $('#white-square').offset();
+  return $('img').offset();
 };
 
 var set_offset = function(offset_top, offset_left) {
-  return $('#white-square').offset({top: offset_top, left: offset_left});
+  return $('img').offset({top: offset_top, left: offset_left});
 };
 
 var log_offset = function() {
@@ -23,7 +23,7 @@ var log_offset = function() {
 
 var log_offset_loop = function() {
   //log_offset();
-  console.log($('#white-square').offset());
+  console.log($('img').offset());
   setTimeout(log_offset_loop, 5000);
   return null
 }
@@ -32,9 +32,50 @@ var log_window_size = function() {
   console.log($(window).height(),', ', $(window).width());
 }
 
-var drift_down = function() {
-    record_offset();
-    set_offset(WhiteSquare.offset_top + 1, WhiteSquare.offset_left);
-    record_offset();
-    setTimeout(drift_down, 1000);
+var width = $(window).width();
+var height = $(window).height();
+var going_down = false
+var going_right = true
+var drift = function() {
+    if (Mooninites.offset_top + 230 < height && going_down) {
+        record_offset();
+        if (Mooninites.offset_left + 300 < width && going_right) {
+            set_offset(Mooninites.offset_top + 1, Mooninites.offset_left + 1);
+        }
+        else {
+            set_offset(Mooninites.offset_top + 1, Mooninites.offset_left -1);
+        }
+            if (Mooninites.offset_top + 230 == height)
+                going_down = false
+            if (Mooninites.offset_left + 300 == width)
+                going_right = false
+            if (Mooninites.offset_top == 0)
+                going_down = true
+            if (Mooninites.offset_left == 0)
+                going_right = true
+        record_offset();
+        setTimeout(drift, 10);
+    }
+    else {
+        record_offset();
+        if (Mooninites.offset_left +300 < width && going_right) {
+            set_offset(Mooninites.offset_top -1, Mooninites.offset_left + 1);
+        }
+        else {
+            set_offset(Mooninites.offset_top -1, Mooninites.offset_left -1);
+        }
+        if (Mooninites.offset_top + 230 == height)
+           going_down = false
+        if (Mooninites.offset_left + 300 == width)
+           going_right = false
+        if (Mooninites.offset_top == 0)
+           going_down = true
+        if (Mooninites.offset_left == 0)
+           going_right = true
+        record_offset();
+        setTimeout(drift, 10);
+    }
 }
+
+
+
