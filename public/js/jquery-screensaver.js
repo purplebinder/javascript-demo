15dd1334327620e0bda667082e1b711e197aaf0c
Emily Feder <emilyafeder@gmail.com>
2013-10-10 10:16:05 -0500

    fixed bug where it was going off the screen

--- public/js/jquery-screensaver.js
+++ public/js/jquery-screensaver.js
@@ -9,30 +9,30 @@
 
 function ScreenSaver (img) {
 
-  var topshift = "+=10px";
-  var leftshift = "-=10px";
+  var topshift = "+=1px";
+  var leftshift = "-=1px";
   var img = img
 
   function animate_img() {
 
-    if (parseFloat($(img).css("bottom"))<=0) {
-      topshift = "-=10px";
+    if (parseFloat($(img).css("bottom"))==0) {
+      topshift = "-=1px";
     }
-    if (parseFloat($(img).css("top"))<=0) {
-      topshift = "+=10px";
+    if (parseFloat($(img).css("top"))==0) {
+      topshift = "+=1px";
     }
-    if (parseFloat($(img).css("left"))<=0) {
-      leftshift = "+=10px";
+    if (parseFloat($(img).css("left"))==0) {
+      leftshift = "+=1px";
     }
-    if (parseFloat($(img).css("right"))<=0) {
-      leftshift = "-=10px";
+    if (parseFloat($(img).css("right"))==0) {
+      leftshift = "-=1px";
     }
-    $(img).animate({top: topshift, left: leftshift}, 100);
+    $(img).animate({top: topshift, left: leftshift}, 10);
   }
 
 
   this.drift = function () {
-    setInterval(animate_img, 100);
+    setInterval(animate_img, 20);
   }
 
 }
