d37fd8289341c6be4567bca97dfbb7089599ac19
Emily Feder <emilyafeder@gmail.com>
2013-10-10 09:56:52 -0500

    fixed Object Oriented bugs.

--- public/js/jquery-screensaver.js
+++ public/js/jquery-screensaver.js
@@ -5,30 +5,29 @@
 
 // Your code goes here.
 //
-//
+
 
 function ScreenSaver (img) {
 
-  this.topshift = "+=10px";
-  this.leftshift = "-=10px";
-  this.img = img
+  var topshift = "+=10px";
+  var leftshift = "-=10px";
+  var img = img
 
   function animate_img() {
 
-    console.log("animate function called");
-    if (parseFloat($(this.img).css("bottom"))<=0) {
-      this.topshift = "-=10px";
+    if (parseFloat($(img).css("bottom"))<=0) {
+      topshift = "-=10px";
     }
-    if (parseFloat($(this.img).css("top"))<=0) {
-      this.topshift = "+=10px";
+    if (parseFloat($(img).css("top"))<=0) {
+      topshift = "+=10px";
     }
-    if (parseFloat($(this.img).css("left"))<=0) {
-      this.leftshift = "+=10px";
+    if (parseFloat($(img).css("left"))<=0) {
+      leftshift = "+=10px";
     }
-    if (parseFloat($(this.img).css("right"))<=0) {
-      this.leftshift = "-=10px";
+    if (parseFloat($(img).css("right"))<=0) {
+      leftshift = "-=10px";
     }
-    $(this.img).animate({top: this.topshift, left: this.leftshift}, 100);
+    $(img).animate({top: topshift, left: leftshift}, 100);
   }
 
 
@@ -37,3 +36,4 @@ function ScreenSaver (img) {
   }
 
 }
+
