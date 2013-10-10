63fb13b16f89a7b0318f65cbe262c73864b4a81e
Declan Frye <deckleberryfrye@gmail.com>
2013-10-10 14:10:05 -0500

    added reference solution for Angular

--- public/js/angular-screensaver.js
+++ public/js/angular-screensaver.js
@@ -1,16 +1,51 @@
 function ScreenSaverController() {
 }
 
+var foo;
+
 var screenSaverApp = window.screenSaverApp = angular.module('screenSaverApp', [])
   .directive('screenSaver', function($timeout) {
     return function(scope, element, attrs) {
-      console.log(scope);
-      console.log(element);
-      console.log(attrs);
+      var iHat = jHat = 1;
+      var height = element[0].clientHeight,
+        width = element[0].clientWidth,
+        screenHeight = document.height,
+        screenWidth = document.width;
+        interval = (attrs.interval || 10);
+
+      element.css({position: 'absolute'});
+
+      function newX() {
+        return element[0].x + (iHat * 1);
+      }
+
+      function newY() {
+        return element[0].y + (jHat * 1);
+      }
+
+      function setDirection() {
+        if (element[0].x <= 0) {
+          iHat = 1;
+        }
+        if (element[0].y <= 0) {
+          jHat = 1;
+        }
+        if ((element[0].x + width) >= screenWidth) {
+          iHat = -1;
+        }
+        if ((element[0].y + height) >= screenHeight) {
+          jHat = -1;
+        }
+      }
+
+      function setPosition(x,y) {
+        element.css({left: x + 'px', top: y + 'px'});
+      }
 
       function incPosition() {
-        element.css({left: (element[0].x + 1) + 'px'});
-        $timeout(incPosition, 1000);
+        setDirection();
+        setPosition(newX(), newY());
+        $timeout(incPosition, interval);
       }
 
       incPosition();
