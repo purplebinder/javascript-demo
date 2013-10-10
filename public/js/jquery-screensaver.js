60d64943f2b987a4288a895c118825c436efc624
Emily Feder <emilyafeder@gmail.com>
2013-10-10 09:32:30 -0500

    initial commit. Has bugs with the OO and that the img doesn't quite stay within the bounds of the screen.

--- public/js/jquery-screensaver.js
+++ public/js/jquery-screensaver.js
@@ -4,3 +4,36 @@
  */
 
 // Your code goes here.
+//
+//
+
+function ScreenSaver (img) {
+
+  this.topshift = "+=10px";
+  this.leftshift = "-=10px";
+  this.img = img
+
+  function animate_img() {
+
+    console.log("animate function called");
+    if (parseFloat($(this.img).css("bottom"))<=0) {
+      this.topshift = "-=10px";
+    }
+    if (parseFloat($(this.img).css("top"))<=0) {
+      this.topshift = "+=10px";
+    }
+    if (parseFloat($(this.img).css("left"))<=0) {
+      this.leftshift = "+=10px";
+    }
+    if (parseFloat($(this.img).css("right"))<=0) {
+      this.leftshift = "-=10px";
+    }
+    $(this.img).animate({top: this.topshift, left: this.leftshift}, 100);
+  }
+
+
+  this.drift = function () {
+    setInterval(animate_img, 100);
+  }
+
+}
